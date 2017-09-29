2e7bda61869ebc3be113b17de0743fa785868645
Mohamed Anwar <mohamed_anwar@opmbx.org>
2017-09-29 02:12:14 +0200

    Add Compass Library

---
+++ libs/Compass/Compass.cpp
@@ -0,0 +1,50 @@
+#include <Compass.h>
+
+Compass::Compass() {
+  Adafruit_HMC5883_Unified(12345);
+
+  if (!begin()) { // to make sure that the connection is right 
+    Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
+    while (1);
+  }
+}
+
+float Compass::getDegree() {
+  sensors_event_t event;
+  getEvent(&event);
+
+  float heading  = atan2(event.magnetic.x, event.magnetic.z);
+  float heading2 = atan2(event.magnetic.y, event.magnetic.x);
+
+  float declinationAngle = 0.22;
+  heading  += declinationAngle;
+  heading2 += declinationAngle;  
+
+  if (heading < 0)
+    heading += 2 * PI;
+  if (heading > 2 * PI)
+    heading -= 2 * PI;
+
+  if (heading2 < 0)
+    heading2 += 2 * PI;
+  if (heading2 > 2 * PI)
+    heading2 -= 2 * PI;
+    
+  headingDegrees = heading * 180 / M_PI;
+  headingDegrees2 = heading2 * 180 / M_PI;
+  float s = 2.3;
+  degree = ((H + headingDegrees) + ((H2 + headingDegrees2) / s));
+
+  if (degree < 0)
+    degree += 2 * 180;
+  if (degree > 2 * 180)
+    degree -= 2 * 180;
+
+  return degree;
+}
+
+void Compass::init() {
+  getDegree();
+  H = -headingDegrees;
+  H2 = -headingDegrees2;
+}
---
+++ libs/Compass/Compass.h
@@ -0,0 +1,16 @@
+#ifndef _COMPASS_H
+#define _COMPASS_H
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>
+
+class Compass:  Adafruit_HMC5883_Unified {
+  float H = 0, H2 = 0, headingDegrees, headingDegrees2, degree;
+public:
+  Compass();
+  void init();
+  float getDegree();
+};
+
+#endif
