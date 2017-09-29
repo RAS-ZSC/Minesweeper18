5ac3646fae60ad826566af43e9b23ee46260f710
Mohamed Anwar <mohamed_anwar@opmbx.org>
2017-09-29 11:32:02 +0200

    Provide skeleton for RotaryEncoder library

--- libs/RotaryEncoder/RotaryEncoder.h
+++ libs/RotaryEncoder/RotaryEncoder.h
@@ -1,17 +1,15 @@
 #ifndef _ROTARY_ENCODER_H
 #define _ROTARY_ENCODER_H
 
-#if 0
-#include <Encoder.h>
 class RotaryEncoder {
 private:
-  Encoder enc1, enc2;
+  //Encoder enc1, enc2;
   float lastDistance, encoderMes;
   long oldPosition, oldPosition2;
+
 public:
-  RotaryEncoder(uint8_t, uint8_t, uint8_t, uint8_t, float);
+  RotaryEncoder();
   float getDistance();
 };
-#endif
 
 #endif
