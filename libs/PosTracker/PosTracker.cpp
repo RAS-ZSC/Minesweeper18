fbd9e1264437aac5b8403db58ff34b5c81b8bab7
Mohamed Anwar <mohamed_anwar@opmbx.org>
2017-10-03 16:38:06 +0200

    Update PosTracker library

--- libs/PosTracker/PosTracker.cpp
+++ libs/PosTracker/PosTracker.cpp
@@ -2,36 +2,30 @@
 #include <EEPROM.h>
 #include <PosTracker.h>
 
-void PosTracker::savePos(float val, int first, int last) {
-  float value = abs(val);
-  for (first; first <= last; first++)
-  {
-    if (value >= 255)
-      EEPROM.write(first , 255);
-    else if (value > 0 && value < 255)
-      EEPROM.write(first, value);
-    else
-      EEPROM.write(first, 0);
-    value = value - 255;
-  }
-  if (val < 0)
-    EEPROM.write(last + 1, 1);
-  else
-    EEPROM.write(last + 1, 0);
+// Store floats
+union float_t {
+  float f;
+  byte b[4];
+} __attribute__((packed));
 
+void PosTracker::savePos(int idx, float val) {
+  float_t v = {.f = val};
+  EEPROM.write(idx*4+0 , v.b[0]);
+  EEPROM.write(idx*4+1 , v.b[1]);
+  EEPROM.write(idx*4+2 , v.b[2]);
+  EEPROM.write(idx*4+3 , v.b[3]);
 }
 
-float PosTracker::findPos(int first, int last) {
-  float pos = 0;
-  for ( first ; first <= last ; first++) {
-    pos = pos + EEPROM.read(first);
-  }
-  if (EEPROM.read(last + 1))
-    pos = pos * (-1);
-  return pos;
+float PosTracker::findPos(int idx) {
+  float_t v;
+  v.b[0] = EEPROM.read(idx*4+0);
+  v.b[1] = EEPROM.read(idx*4+1);
+  v.b[2] = EEPROM.read(idx*4+2);
+  v.b[3] = EEPROM.read(idx*4+3);
+  return v.f;
 }
 
 void PosTracker::clear() {
-  for (int i = 0; i < 100; ++i)
+  for (int i = 0; i < EEPROM.length(); ++i)
     EEPROM.write(i, 0);
 }
--- libs/PosTracker/PosTracker.h
+++ libs/PosTracker/PosTracker.h
@@ -2,12 +2,10 @@
 #define _POS_TRACKER_H
 
 class PosTracker {
-  int base, limit;
 public:
-  PosTracker(int, int);
   void clear();
-  void savePos(float, int, int);
-  float findPos(int, int);
+  void savePos(int, float);
+  float findPos(int);
 };
 
 #endif
