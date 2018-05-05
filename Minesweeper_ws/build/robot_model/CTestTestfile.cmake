8a1299ea398f621aed1685cfefd8f315cc54c6c4
HazemGabr232 <32387240+HazemGabr232@users.noreply.github.com>
2018-05-05 21:25:08 +0200

    Add files via upload

---
+++ Minesweeper_ws/build/robot_model/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hazem/Minesweeper_ws/src/robot_model
+# Build directory: /home/hazem/Minesweeper_ws/build/robot_model
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Minesweeper_ws/src/joypkg/scripts/JoyControl.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python2.7
+from __future__ import division
+from std_msgs.msg import Int8, String
+import rospy
+import pygame
+#import serial 
+import struct
+from math import *
+
+# Key mappings
+PS3_BUTTON_A = 0
+PS3_BUTTON_B = 1
+PS3_BUTTON_X = 2
+PS3_BUTTON_Y = 3
+PS3_BUTTON_LB = 4
+PS3_BUTTON_RB = 5
+PS3_BUTTON_LT = 6
+PS3_BUTTON_RT = 7
+PS3_BUTTON_Back = 8
+PS3_BUTTON_Start = 9
+
+
+#Start the communication with Arduino 
+#arduino = serial.Serial('/dev/ttyACM0', 9600)
+
+#Start pygame 
+pygame.init()
+
+#Start ROS Node
+rospy.init_node('Joystick_node', anonymous=True)
+pub = rospy.Publisher('Joystick_', String, queue_size = 10 )
+
+# Wait for a joystick
+while pygame.joystick.get_count() == 0:
+    print ("waiting for joystick")
+    rospy.Rate(1).sleep()    
+    pygame.joystick.get_count()
+    pygame.joystick.quit()
+    pygame.joystick.init()
+    
+#Start joystick connection 
+j = pygame.joystick.Joystick(0)
+j.init()
+print (j.get_name(), " is connected")
+
+#joystick information
+joystick_count = pygame.joystick.get_count()
+print("joystick_count")
+print(joystick_count)
+print("--------------")
+ 
+numaxes = j.get_numaxes()
+print("numaxes")
+print(numaxes)
+print("--------------")
+ 
+numbuttons = j.get_numbuttons()
+print("numbuttons")
+print(numbuttons)
+print("--------------")
+ 
+X = 0
+Y = 0
+res = 255
+# Mappinng butttons    
+def buttonscontrol(event):
+    global X
+    global Y
+
+    if event.type == pygame.JOYAXISMOTION:
+        if event.axis > 1:
+            return
+        if event.axis is 0:
+            X =  int(res * event.value)
+        elif event.axis is 1:
+            Y = -int(res * event.value)
+
+        r = sqrt(X**2 + Y**2)
+        r = res if r > res else r
+        th = atan2(abs(Y), abs(X)) * 4 / 3.141592653589793 * r - r
+
+        r  = int(ceil(r))
+        th = int(ceil(th))
+
+        L = 0
+        R = 0
+        if X >= 0 and Y >= 0:   #First Quadrant
+            L = r
+            R = th
+        elif X < 0 and Y >= 0:  #Second Quadrant
+            L = th
+            R = r
+        elif X < 0 and Y < 0:   #Third Quadrant
+            L = -r
+            R = -th
+        else:
+            L = -th
+            R = -r
+        
+        print (L, R)
+        #print (X, Y)
+      #  arduino.write(b"S")
+      #  arduino.write(struct.pack(">hh", L, R))
+   # return
+
+  #  print(event)
+    Mode = ""
+    if event.type == pygame.JOYAXISMOTION and event.value < 0 and event.axis == 0:
+        print("AXIS_LEFT_HORIZONTAL L")
+        #Publish in ROS 
+        #rospy.loginfo('left')
+        #pub.publish('L')
+        #Send through serial 
+        Mode = 'L'
+			
+    elif event.type == pygame.JOYAXISMOTION and event.value < 0 and event.axis == 1:
+        print("AXIS_LEFT_VERTICAL U")
+        #Publish in ROS 
+        #rospy.loginfo('Up')
+        #pub.publish('U')
+        #Send through serial 
+        Mode = 'U'
+	
+    elif event.type == pygame.JOYAXISMOTION and event.value == 1 and event.axis == 0:
+        print("AXIS_LEFT_HORIZONTAL R")
+        #Publish in ROS 
+        #rospy.loginfo('Right')
+        #pub.publish('R')
+        #Send through serial 
+        Mode = 'R'
+	
+    elif event.type == pygame.JOYAXISMOTION and event.value == 1 and event.axis == 1:
+        print("AXIS_LEFT_VERTICAL D")
+        #Publish in ROS 
+        #rospy.loginfo('Down')
+        #pub.publish('D')
+        #Send through serial 
+        Mode = 'D'
+	
+    if event.type == pygame.JOYBUTTONDOWN:
+        if event.button == PS3_BUTTON_X:
+            print("BUTTON_X")
+            #rospy.loginfo('X')
+            #pub.publish('X')
+            Mode = 'X'
+            
+        elif event.button == PS3_BUTTON_Y:        ###LEFT (Y)
+            print("BUTTON_Y")
+            #rospy.loginfo('Y')
+            pub.publish('Y')
+            Mode = 'Y'
+        elif event.button ==PS3_BUTTON_B:          ### RIGHT (B)
+            print("BUTTON_B")
+            #rospy.loginfo('B')
+            pub.publish('B')
+            Mode= 'B'
+        elif event.button == PS3_BUTTON_A:        ###DOWN (A) 
+            print("BUTTON_A")
+            #rospy.loginfo('A')
+            pub.publish('A')
+            Mode= 'A'
+        elif event.button == PS3_BUTTON_RB :
+            print("BUTTON_RB")
+            #rospy.loginfo('RB')
+            #pub.publish('M')
+            Mode = 'M'
+        elif event.button == PS3_BUTTON_RT :
+            print("BUTTON_RT")
+            #rospy.loginfo('RT')
+            #pub.publish('N')
+            Mode = 'N'
+        elif event.button == PS3_BUTTON_LB :         #### UP (Y)
+            print("BUTTON_LB")
+            #rospy.loginfo('LB')
+            pub.publish('P')
+            Mode = 'P'
+        elif event.button == PS3_BUTTON_LT :
+            print("BUTTON_LT")
+            #rospy.loginfo('LT')
+            #pub.publish('P')
+            Mode = 'P'
+        elif event.button == PS3_BUTTON_Back :
+            print("BUTTON_Back")
+            #rospy.loginfo('Back')
+            #pub.publish('K')
+            Mode = 'K'
+        elif event.button == PS3_BUTTON_Start:
+            print("BUTTON_Start")
+            #rospy.loginfo('Start')
+            pub.publish('S')
+            Mode = 'S'
+    elif event.type == pygame.JOYBUTTONUP:  ### STOP WHEN NO BUTTON IS PRESSED 
+        print("STOP")
+        pub.publish('Q')
+        Mode = 'Q'
+            
+   # arduino.write(Mode)
+    rospy.Rate(100).sleep()
+        
+
+#Announcment for start 
+#rospy.loginfo('Begin')
+pub.publish(0000)
+
+while True:
+    #read in joystick events
+    events = pygame.event.get()
+   
+    # and process them
+    for event in events:
+        buttonscontrol(event)
+
+    #while arduino.in_waiting > 0:
+    #    print arduino.read()
