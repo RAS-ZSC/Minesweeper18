37c68d25c2cc89f03db6819314e8d1ca4c742416
HazemGabr232 <32387240+HazemGabr232@users.noreply.github.com>
2018-07-04 13:30:44 +0200

    Add files via upload

--- Robot/Minesweeper.ino
+++ Robot/Minesweeper.ino
@@ -1,42 +1,74 @@
-
-float ldistance ;
-float rdistance ;
-float distance ;
-float theta ;
-float lastDistance=0 ;
-float x ;
-float y ;
 #include <ros.h>
 #include <ros/time.h>
 #include <tf/tf.h>
 #include <tf/transform_broadcaster.h>
-#include <RotaryEncoder.h>
-RotaryEncoder lencoder(0, 0.025), rencoder(1, 0.025);
-=======
-#define BLUETOOTH_CTRL  1
-#define JOYSTICK_CTRL   1
+int marker_id=0;
+float sonic_distance=300 ; //calibrate ultrasonic then change this 
+long _PreviousLeftEncoderCounts = 0;
+long _PreviousRightEncoderCounts = 0;
+//ros::Time 
+//long current_time, last_time;
+ros::Time current_time;
+ros::Time last_time;
 
-#include <HBridge.h>
-HBridge hbridge(6, 7, 4, 5, 10, 11);
+double DistancePerCount = (3.14159265 * 0.062)/500;
 
-/* Control Mehtods */
-#if BLUETOOTH_CTRL
-#include <SoftwareSerial.h>
-SoftwareSerial bluetooth(50, 51);
-#endif
+//unsigned long current_time , last_time ;
 
-#if JOYSTICK_CTRL
-#include <JoyControl.h>
-JoyControl joystick(hbridge);
-#endif
 
-/* Compass Connections
- * UNO: SDA -> A4, SCL -> A5
- * MEGA: SDA -> 20, SCL -> 21
- * LEO: SDA -> 2, SCL -> 3
- */
-#include <Compass.h>
-Compass compass;
+double x; 
+double y;
+double th;
+double v_left;//left motor speed
+double v_right;//right motor speed
+double vth;//angular velocity of robot
+double deltaLeft;//no of ticks in left encoder since last update
+double deltaRight;//no of ticks in right encoder since last update
+double dt;
+double delta_distance;//distance moved by robot since last update
+double delta_th;//corresponging change in heading
+double delta_x ;//corresponding change in x direction
+double delta_y;//corresponding change in y direction
+#define PI 3.14159265
+#define TwoPI 6.28318531
+/******************************************/
+
+int encoderPin1 = 3;
+int encoderPin2 = 2;
+int encoderPin3 = 21;
+int encoderPin4 = 20;
+
+
+volatile int lastEncoded1 = 0;
+volatile long encoderValue1 = 0;
+volatile int lastEncoded2 = 0;
+volatile long encoderValue2 = 0;
+
+//long lastencoderValue1 = 0;
+
+//long lastencoderValue2 = 0;
+
+
+int lastMSB1 = 0;
+int lastLSB1 = 0;
+int lastMSB2 = 0;
+int lastLSB2 = 0;
+
+
+
+
+
+
+#include <visualization_msgs/Marker.h>
+visualization_msgs::Marker marker;
+ros::Publisher mark("mark", &marker);
+
+#include <MetalDetector.h>	
+MetalDetector lmetal(9), rmetal(10);
+
+#include <Ultrasonic.h>
+Ultrasonic    lsonic(/*echo_pin*/14,/*trig_pin*/15), rsonic(/*echo_pin*/16,/*trig_pin*/17);
+
 
 
 ros::NodeHandle  nh;
@@ -51,99 +83,210 @@ char odom[] = "/odom";
 
 void setup()
 {
+
+pinMode(encoderPin1, INPUT);
+pinMode(encoderPin2, INPUT);
+pinMode(encoderPin3, INPUT);
+pinMode(encoderPin4, INPUT);
+
+digitalWrite(encoderPin1, HIGH); //turn pullup resistor on
+digitalWrite(encoderPin2, HIGH); //turn pullup resistor on
+digitalWrite(encoderPin3, HIGH); //turn pullup resistor on
+digitalWrite(encoderPin4, HIGH); //turn pullup resistor on
+
+
+
+//call updateEncoder() when any high/low changed seen
+//on interrupt 0 (pin 2), or interrupt 1 (pin 3)
+attachInterrupt(0, updateEncoder, CHANGE);
+attachInterrupt(1, updateEncoder, CHANGE);
+attachInterrupt(2, updateEncoder, CHANGE);
+attachInterrupt(3, updateEncoder, CHANGE);
+
+
   nh.initNode();
+  nh.advertise(mark);
   broadcaster.init(nh);
 }
 
 void loop()
 {  
-    ldistance = lencoder.getDistance();
-   rdistance = rencoder.getDistance();
-   distance = (ldistance + rdistance) / 2;
 
-   theta = (compass.getDegree() * PI) / 180;
+  nh.spinOnce();
 
-  x += (distance - lastDistance) * cos(theta);
-  y += (distance - lastDistance) * sin(theta);
+ current_time = nh.now();
 
-  lastDistance = distance;
-  
+ deltaLeft = encoderValue1 -_PreviousLeftEncoderCounts;
+ deltaRight = encoderValue2 - _PreviousRightEncoderCounts;
+ 
+  dt = (current_time.toSec() - last_time.toSec());
+ v_left = deltaLeft * DistancePerCount/dt;
+ v_right = deltaRight * DistancePerCount/dt;
+ delta_distance=0.5f*(double)(deltaLeft+deltaRight)*DistancePerCount;
+ delta_th = (double)(deltaRight-deltaLeft)*DistancePerCount/0.36f; //Distance between the two wheels is 0.36m
+ delta_x = delta_distance*(double)cos(th);
+ delta_y = delta_distance*(double)sin(th);
+ x += delta_x;
+ y += delta_y;
+ th += delta_th;
+ if (th > PI)
+ th -= TwoPI;
+ else
+ if ( th <= -PI)
+ th += TwoPI;
+ _PreviousLeftEncoderCounts = encoderValue1;
+ _PreviousRightEncoderCounts = encoderValue2;
+
+   
+if (lmetal.detect()==1){
+   if (lsonic.getDistance() < sonic_distance){
+visualization_msgs::Marker marker;
+ marker.header.frame_id = "base_link";
+ marker.header.stamp = ros::Time();
+ marker.ns = "my_namespace";
+ marker.id = marker_id;
+ marker.type = visualization_msgs::Marker::SPHERE; //if the mine above the ground will be shown as a sphere 
+ marker.action = visualization_msgs::Marker::ADD;
+ marker.pose.position.x = 1;
+ marker.pose.position.y = 1;
+ marker.pose.position.z = 1;
+ marker.pose.orientation.x = 0.0;
+ marker.pose.orientation.y = 0.0;
+ marker.pose.orientation.z = 0.0;
+ marker.pose.orientation.w = 1.0;
+ marker.scale.x = 1;
+ marker.scale.y = 0.1;
+ marker.scale.z = 0.1;
+ marker.color.a = 1.0; // Don't forget to set the alpha!
+ marker.color.r = 0.0;
+ marker.color.g = 1.0;
+ marker.color.b = 0.0;
+//only if using a MESH_RESOURCE marker type:
+// marker.mesh_resource = "package://pr2_description/meshes/base_v0/base.dae";
+ mark.publish( &marker );
+marker_id++;
+}   
+   else{
+ visualization_msgs::Marker marker;
+ marker.header.frame_id = "base_link";
+ marker.header.stamp = ros::Time();
+ marker.ns = "my_namespace";
+ marker.id = marker_id;
+ marker.type = visualization_msgs::Marker::CUBE; //if the mine under the ground will be shown as a cube
+ marker.action = visualization_msgs::Marker::ADD;
+ marker.pose.position.x = 1;
+ marker.pose.position.y = 1;
+ marker.pose.position.z = 1;
+ marker.pose.orientation.x = 0.0;
+ marker.pose.orientation.y = 0.0;
+ marker.pose.orientation.z = 0.0;
+ marker.pose.orientation.w = 1.0;
+ marker.scale.x = 1;
+ marker.scale.y = 0.1;
+ marker.scale.z = 0.1;
+ marker.color.a = 1.0; // Don't forget to set the alpha!
+ marker.color.r = 0.0;
+ marker.color.g = 1.0;
+ marker.color.b = 0.0;
+//only if using a MESH_RESOURCE marker type:
+// marker.mesh_resource = "package://pr2_description/meshes/base_v0/base.dae";
+ mark.publish( &marker );
+marker_id++;
+}
+}
+
+ if (rmetal.detect()==1){   //put another if for ultrasonic with another different marker 
+    if (rsonic.getDistance() < sonic_distance){
+visualization_msgs::Marker marker;
+ marker.header.frame_id = "base_link";
+ marker.header.stamp = ros::Time();
+ marker.ns = "my_namespace";
+ marker.id = marker_id;
+ marker.type = visualization_msgs::Marker::SPHERE;  //if the mine above the ground will be shown as a sphere
+ marker.action = visualization_msgs::Marker::ADD;
+ marker.pose.position.x = 1;
+ marker.pose.position.y = 1;
+ marker.pose.position.z = 1;
+ marker.pose.orientation.x = 0.0;
+ marker.pose.orientation.y = 0.0;
+ marker.pose.orientation.z = 0.0;
+ marker.pose.orientation.w = 1.0;
+ marker.scale.x = 1;
+ marker.scale.y = 0.1;
+ marker.scale.z = 0.1;
+ marker.color.a = 1.0; // Don't forget to set the alpha!
+ marker.color.r = 0.0;
+ marker.color.g = 1.0;
+ marker.color.b = 0.0;
+//only if using a MESH_RESOURCE marker type:
+// marker.mesh_resource = "package://pr2_description/meshes/base_v0/base.dae";
+ mark.publish( &marker );
+marker_id++;
+}
+else{
+ visualization_msgs::Marker marker;
+ marker.header.frame_id = "base_link";
+ marker.header.stamp = ros::Time();
+ marker.ns = "my_namespace";
+ marker.id = marker_id;
+ marker.type = visualization_msgs::Marker::CUBE; //if the mine under the ground will be shown as a cube
+ marker.action = visualization_msgs::Marker::ADD;
+ marker.pose.position.x = 1;
+ marker.pose.position.y = 1;
+ marker.pose.position.z = 1;
+ marker.pose.orientation.x = 0.0;
+ marker.pose.orientation.y = 0.0;
+ marker.pose.orientation.z = 0.0;
+ marker.pose.orientation.w = 1.0;
+ marker.scale.x = 1;
+ marker.scale.y = 0.1;
+ marker.scale.z = 0.1;
+ marker.color.a = 1.0; // Don't forget to set the alpha!
+ marker.color.r = 0.0;
+ marker.color.g = 1.0;
+ marker.color.b = 0.0;
+//only if using a MESH_RESOURCE marker type:
+// marker.mesh_resource = "package://pr2_description/meshes/base_v0/base.dae";
+ mark.publish( &marker );
+marker_id++;
+}
+} 
     
   // tf odom->base_link
   t.header.frame_id = odom;
-  t.child_frame_id = base_link;
+  t.child_frame_id = "minesweeperUdrf";
   
   t.transform.translation.x = x;
   t.transform.translation.y = y;
   
-  t.transform.rotation = tf::createQuaternionFromYaw(theta);
+  t.transform.rotation = tf::createQuaternionFromYaw(th);
   t.header.stamp = nh.now();
   
   broadcaster.sendTransform(t);
+  
   nh.spinOnce();
   
   delay(10);
-=======
-/* Constructor: RotaryEncoder(id, mps) ->  mps is Meters Per Second 
- * Connections: Interrupt Pins 
- */
-
-//#include <RotaryEncoder.h>
-//RotaryEncoder lencoder(0, 0.025), rencoder(1, 0.025);
-
-//#include <MetalDetector.h>
-//MetalDetector rdetector(10), ldetector(11);
-
-
-//#include <Ultrasonic.h>
-//Ultrasonic usonic(10, 9);
-
-// Custom Libs
-//#include <Mapper.h>
-//#include <RF.h>
-
-//Mapper mapper(encoder, compass, usonic);
-
-//RF radio(49, 48);
-
-void setup() {
-  Serial.begin(9600);
-  //bluetooth.begin(9600);
-  //compass.begin();
-  //compass.init();
-  
-  //radio.begin(1);
 }
 
 
-void loop() {
-#if BLUETOOTH_CTRL
-  if (bluetooth.available() > 0) {
-    int code = bluetooth.read();
-
-    switch (code) {
-      case 0x53: hbridge.stop(); break;
-      case 0x46: hbridge.forward(); break;
-      case 0x42: hbridge.backward(); break;
-      case 0x52: hbridge.right(); break;
-      case 0x4C: hbridge.left(); break;
-    }
-  }
-#endif
-
-    /* Your code goes here */
-}
 
 
-/*
- * Arduino attaches 'serialEvent()' to 'loop()' function, so using delays
- * inside loop will delay this function
- */
-void serialEvent()
-{
-#if JOYSTICK_CTRL
-  byte code = Serial.read();
-  joystick.handle(code);
-#endif
+void updateEncoder(){
+int MSB1 = digitalRead(encoderPin1); //MSB = most significant bit
+int LSB1 = digitalRead(encoderPin2); //LSB = least significant bit
+int MSB2 = digitalRead(encoderPin3); //MSB = most significant bit
+int LSB2 = digitalRead(encoderPin4); //LSB = least significant bit
 
-}
+int encoded1 = (MSB1 << 1) |LSB1; //converting the 2 pin value to single number
+int encoded2 = (MSB2 << 1) |LSB2;
+int sum1 = (lastEncoded1 << 2) | encoded1; //adding it to the previous encoded value
+int sum2 = (lastEncoded2 << 2) | encoded2;
+if(sum1 == 0b1101 || sum1 == 0b0100 || sum1 == 0b0010 || sum1 == 0b1011) encoderValue1 ++; 
+if(sum2 == 0b1101 || sum2 == 0b0100 || sum2 == 0b0010 || sum2 == 0b1011) encoderValue2 ++; 
+
+if(sum1 == 0b1110 || sum1 == 0b0111 || sum1 == 0b0001 || sum1 == 0b1000) encoderValue1--; 
+if(sum2 == 0b1110 || sum2 == 0b0111 || sum2 == 0b0001 || sum2 == 0b1000) encoderValue2 --; 
+lastEncoded2 = encoded2; //store this value for next time 
+lastEncoded1 = encoded1; //store this value for next time 
+} 
